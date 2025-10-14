5ee26a2212847a269c634fbb0a51cba6545a2516
sonubabut <tsonubabu0@gmail.com>
2025-10-14 17:40:23 +0530

    branding dt3

--- src/config/defaultConfig.ts
+++ src/config/defaultConfig.ts
@@ -2,9 +2,9 @@ export const DEFAULT_CONFIG = {
   server: {
     rest: {
       // baseURL: "https://d36gcxtyqpt9hg.cloudfront.net/api/", // Prod url
-      // baseURL: "https://d3onkomwx8of5j.cloudfront.net/api/", // Dev url
-      //baseURL: "http://localhost:5000/api/", // Local url
-        baseURL: "https://sbh3fg3j-5000.asse.devtunnels.ms/api/", 
+      baseURL: "https://d3onkomwx8of5j.cloudfront.net/api/", // Dev url
+      // baseURL: "http://localhost:5000/api/", // Local url
+        // baseURL: "https://sbh3fg3j-5000.asse.devtunnels.ms/api/", 
     },
   },
 };
